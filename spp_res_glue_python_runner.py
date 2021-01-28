5aba3a57e5ad2a42e5c2a43dbe6c47c676d6bd37
Rhodri Guerrier <rhodriguerrier@Rhodris-MacBook-Pro.local>
2021-01-28 13:07:10 +0000

    parse logger to pipeline and get rid of survey and try blocks

--- spp_res_glue_python_runner.py
+++ spp_res_glue_python_runner.py
@@ -11,7 +11,6 @@ from spp.engine.pipeline import construct_pipeline
 from awsglue.utils import getResolvedOptions
 from es_aws_functions import general_functions
 
-current_module = "spp-res_glu_python_job_script"
 args = getResolvedOptions(sys.argv, ["config", "crawler"])
 config_str = base64.b64decode(args["config"].encode("ascii")).decode("ascii")
 config = json.loads(config_str)
@@ -20,18 +19,15 @@ environment = config["pipeline"]["environment"]
 run_id = config["pipeline"]["run_id"]
 survey = config["survey"]
 config = config["pipeline"]
-try:
-    logger = general_functions.get_logger(survey, current_module, environment, run_id)
-except Exception as e:
-    raise e
+
+logger = general_functions.get_logger(survey, "spp-results-pipeline", environment, run_id)
 
 try:
     logger.info("Config variables loaded.")
-    pipeline = construct_pipeline(config, survey)
+    pipeline = construct_pipeline(config, logger=logger)
     logger.info("Running pipeline {}, run {}".format(pipeline.name, config["run_id"]))
     pipeline.run(
         crawler_name=crawler
     )
 except Exception as e:
     logger.error("Error constructing and/or running pipeline: ", e)
-    raise e
